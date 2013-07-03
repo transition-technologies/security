b46fad1fe289655058bbe902a4f791900c9cd75e
Marek Piechut <marek.piechut@gmail.com>
2013-07-03 10:50:04 +0200

    More constructors for SecurityException

--- app/security/exception/SecurityException.java
+++ app/security/exception/SecurityException.java
@@ -1,20 +1,43 @@
 package security.exception;
 
 
+import security.AclManaged;
+
+import java.lang.reflect.Method;
+
 public class SecurityException extends RuntimeException {
 
     private static final long serialVersionUID = -3609286207240970458L;
+    private final AclManaged[] forbiddenObjects;
+    private final Method forbiddenMethod;
 
     public SecurityException() {
-        super();
+        this(null);
     }
 
     public SecurityException(String message) {
-        super(message);
+        this(message, null);
     }
 
     public SecurityException(String message, Throwable cause) {
-        super(message, cause);
+        this(message, cause, null);
     }
 
+    public SecurityException(String message, Throwable cause, Method forbiddenMethod, AclManaged... forbiddenObjects){
+        super(message, cause);
+        this.forbiddenMethod = forbiddenMethod;
+        this.forbiddenObjects = forbiddenObjects;
+    }
+
+    public SecurityException(String message, Method forbiddenMethod, AclManaged[] forbiddenObjects) {
+        this(message, null, forbiddenMethod, forbiddenObjects);
+    }
+
+    public Method getForbiddenMethod() {
+        return forbiddenMethod;
+    }
+
+    public AclManaged[] getForbiddenObjects() {
+        return forbiddenObjects;
+    }
 }
